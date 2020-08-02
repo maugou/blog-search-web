93fdb403b3623972dd315c6bd8325b624cbad139
Jo Jae-hoon <dev.hoony@gmail.com>
2020-08-02 22:15:44 +0900

    타입 수정

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,19 @@
 /// <reference types="react-scripts" />
+
+interface SearchResult {
+  meta: {
+    total_count: number;
+    pageable_count: number;
+    is_end: boolean;
+  };
+  documents: [
+    {
+      title: string;
+      contents: string;
+      url: string;
+      blogname: string;
+      thumbnail: string;
+      datetime: Date;
+    }
+  ];
+}
